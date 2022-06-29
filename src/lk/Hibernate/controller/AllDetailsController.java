efcb834a7cd4055c514174bf80f789c33bb1d28d
hasindi <hmudithya@gmail.com>
2022-06-29 16:03:43 +0530

    Design RegisterDetails Form

--- src/lk/Hibernate/controller/AllDetailsController.java
+++ src/lk/Hibernate/controller/AllDetailsController.java
@@ -1,4 +1,21 @@
 package lk.Hibernate.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class AllDetailsController {
+    public TableView tblRegisterDetails;
+    public TableColumn colRegId;
+    public TableColumn colRegDate;
+    public TableColumn colSId;
+    public TableColumn colSName;
+    public TableColumn colContact;
+    public TableColumn colOption;
+    public JFXTextField txtRoomType;
+    public JFXComboBox cmbRoomId;
+    public JFXTextField txtKeyMoney;
+    public JFXTextField txtQuntity;
+
 }
